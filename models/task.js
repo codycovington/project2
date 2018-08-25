9ddbdcf8dc4feee6844365b62072043311fd0b05
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-08-25 18:07:03 -0400

    models made for achievements, users, and tasks. routing updated for the Users and Task

---
+++ models/task.js
@@ -0,0 +1,28 @@
+module.exports = function (sequelize, DataTypes) {
+  var Task = sequelize.define("Task", {
+    text: {
+      type: DataTypes.STRING,
+      // this field cannot be NULL
+      allowNull: false,
+      //this validation makes it be so that the task cannot be blank
+      validate: {
+        len: [1]
+      }
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      //Starting with false means that an action has to be taken to complete this task
+      defaultValue:false
+    }
+  });
+
+  Task.associate = function(models){
+    // The Task needs to belong to a User
+    Task.belongsTo(models.User, {
+      foreignKey: { 
+        allowNull: false
+      }
+    });
+  };
+  return Task;
+};
