d3964774a1d7e481713cd20290bcd7ff6c82feec
Cody Covington <codebase.covington@gmail.com>
2018-08-27 08:17:40 -0400

    Added a project model and updated User.js with association to projects

--- models/user.js
+++ models/user.js
@@ -3,6 +3,14 @@ module.exports = function (sequelize, DataTypes) {
         name: DataTypes.STRING
     });
 
+    //association between the User and the Project
+    User.associate = function (models) {
+        User.hasMany(models.Project, {
+            onDelete: "cascade"
+        });
+    };
+    return User;
+
     //association between the User and the Task
     User.associate = function (models) {
         User.hasMany(models.Task, {
