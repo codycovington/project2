8506b1854239a582ac139564dedc014aa3ce482c
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-09-06 20:24:45 -0400

    users

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,179 +1,185 @@
-//============================================================================//
-// Project logic
+$(document).ready(function () {
+    //============================================================================//
+    // Project logic
 
-var $newProject = $("#new-project");
-// var $userProject = $("#user-project");
-// handles the input in the user-form class and enters the id  new-user to put it into the database
-$(".project-form").on("submit", function (event) {
-    event.preventDefault();
+    var $newProject = $("#new-project");
+    // var $userProject = $("#user-project");
+    // handles the input in the user-form class and enters the id  new-user to put it into the database
+    $(".project-form").on("submit", function (event) {
+        event.preventDefault();
 
-    // this prevents premature submission without the required fields
-    if (!$newProject.val().trim()) {
-        alert("Please complete all fields in order to move forward.");
-        return
-    }
-    var newProject = {
-        project_Name: $newProject.val().trim()
-    };
-    console.log(newProject)
+        // this prevents premature submission without the required fields
+        if (!$newProject.val().trim()) {
+            alert("Please complete all fields in order to move forward.");
+            return
+        }
+        var newProject = {
+            project_Name: $newProject.val().trim()
+        };
+        console.log(newProject)
 
-    $.ajax("/api/projects", {
-        type: "POST",
-        data: newProject
-    }).then(function () {
-        console.log("New Project added: " + newProject)
-        location.reload();
+        $.ajax("/api/projects", {
+            type: "POST",
+            data: newProject
+        }).then(function () {
+            console.log("New Project added: " + newProject)
+            location.reload();
+        });
     });
-});
 
-// handles deleting the user
-$(".delete").on("click", function () {
-    var id = $(this).data(id);
+    // handles deleting the user
+    $(".delete").on("click", function () {
+        var id = $(this).data(id);
 
-    console.log(id.id)
+        console.log(id.id)
 
-    $.ajax("/api/projects/" + id.id, {
-        type: "DELETE",
-        url: "/api/projects/" + id.id
-    }).then(function () {
-        location.reload();
+        $.ajax("/api/projects/" + id.id, {
+            type: "DELETE",
+            url: "/api/projects/" + id.id
+        }).then(function () {
+            location.reload();
+        });
     });
-});
 
 
 
-// ========================================================================//
-//user logic
+    // ========================================================================//
+    //user logic
+    $(".login").on("click", function () {
+        var id = $(this).data(id);
+        window.location.href = `/kanban/${id.id}`;
 
-// Get references to page elements
-var $newUser = $("#new-user");
-var $userProjectId = $("#project-id");
-
-
-// handles the input in the user-form class and enters the id  new-user to put it into the database
-$(".user-form").on("submit", function (event) {
-    event.preventDefault();
-
-    // this prevents premature submission without the required fields
-    if (!$newUser.val().trim() || !$userProjectId.val().trim()) {
-        alert("Please complete all fields in order to move forward.");
-        return
-    }
-
-    var newUser = {
-        name: $newUser.val().trim(),
-        ProjectId: $userProjectId.val().trim()
-    };
-    console.log(newUser)
-
-    $.ajax("/api/users", {
-        type: "POST",
-        data: newUser
-    }).then(function () {
-        console.log("New user added: " + newUser)
-        location.reload();
     });
-});
 
-// handles deleting the user
-$(".delete").on("click", function () {
-    var id = $(this).data(id);
-
-    console.log(id.id)
-
-    $.ajax("/api/users/" + id.id, {
-        type: "DELETE",
-        url: "/api/users/" + id.id
-    }).then(function () {
-        location.reload();
-    })
-});
+    // Get references to page elements
+    var $newUser = $("#new-user");
+    // var $userProjectId = $("#project-id");
 
 
-// ========================================================================//
-// Task Logic
-$("#getValue").click(function (event) {
-    event.preventDefault();
-    var selValue = $(".dropdown").val();
-    console.log(selValue)
+    // handles the input in the user-form class and enters the id  new-user to put it into the database
+    $(".user-form").on("submit", function (event) {
+        event.preventDefault();
 
+        // this prevents premature submission without the required fields
+        if (!$newUser.val().trim()) {
+            alert("Please complete all fields in order to move forward.");
+            return
+        }
 
-    var $newTask = $("#new-task");
-    var $taskCategory = selValue;
-    var $UserId = $("#user-task");
-    console.log("Category: " + $taskCategory)
-    // handles the input in the task-form class and enters the id  task-user to put it into the database
+        var newUser = {
+            name: $newUser.val().trim()
+            // ProjectId: $userProjectId.val().trim()
+        };
+        console.log(newUser)
 
-
-    console.log($taskCategory)
-
-    // this prevents premature submission without the required fields
-    if (!$newTask.val().trim() || !$taskCategory || !$UserId) {
-        alert("Please complete all fields in order to move forward.");
-        return
-    };
-
-    var newTask = {
-        task_name: $newTask.val().trim(),
-        category: $taskCategory,
-        UserId: $UserId.val().trim()
-    };
-    console.log(newTask)
-
-    $.ajax("/api/tasks", {
-        type: "POST",
-        data: newTask
-    }).then(function () {
-        console.log("New task added: " + newTask)
-        location.reload();
+        $.ajax("/api/users", {
+            type: "POST",
+            data: newUser
+        }).then(function () {
+            console.log("New user added: " + newUser)
+            location.reload();
+        });
     });
-});
 
+    // handles deleting the user
+    $(".delete").on("click", function () {
+        var id = $(this).data(id);
 
+        console.log(id.id)
 
-// handles deleting the task
-$(".delete").on("click", function () {
-    var id = $(this).data(id);
-
-    console.log(id.id)
-
-    $.ajax("/api/tasks/" + id.id, {
-        type: "DELETE",
-        url: "/api/tasks/" + id.id
-    }).then(function () {
-        console.log(id + " has been deleted")
-        location.reload();
+        $.ajax("/api/users/" + id.id, {
+            type: "DELETE",
+            url: "/api/users/" + id.id
+        }).then(function () {
+            location.reload();
+        })
     });
-});
-
-//handles the updating of a task
-$(".updateValue").click(function (event) {
-    var id = $(this).data(id);
-    var selValue = $(".update_task").val();
-    console.log(id.id)
-    console.log(selValue)
 
 
-    var $updateCategory = selValue;
-    var $updateUser = $("#update-user");
+    // ========================================================================//
+    // Task Logic
+    $("#getValue").click(function (event) {
+        event.preventDefault();
+        var selValue = $(".dropdown").val();
+        console.log(selValue)
 
-    var update = {
-        id: id.id,
-        category: $updateCategory
-    };
-    console.log("id updated: " + id.id);
-    console.log("category updated: " + $updateCategory);
 
-    $.ajax("/api/tasks/" + id.id, {
-        method: "PUT",
-        url: "/api/tasks/" + id.id,
-        data: update
-    }).then(function () {
-        console.log("updated");
-        window.location.reload();
+        var $newTask = $("#new-task");
+        var $taskCategory = selValue;
+        var $UserId = $("#user-task");
+        console.log("Category: " + $taskCategory)
+        // handles the input in the task-form class and enters the id  task-user to put it into the database
+
+
+        console.log($taskCategory)
+
+        // this prevents premature submission without the required fields
+        if (!$newTask.val().trim() || !$taskCategory || !$UserId) {
+            alert("Please complete all fields in order to move forward.");
+            return
+        };
+
+        var newTask = {
+            task_name: $newTask.val().trim(),
+            category: $taskCategory,
+            UserId: $UserId.val().trim()
+        };
+        console.log(newTask)
+
+        $.ajax("/api/tasks", {
+            type: "POST",
+            data: newTask
+        }).then(function () {
+            console.log("New task added: " + newTask)
+            location.reload();
+        });
     });
-})
 
 
+
+    // handles deleting the task
+    $(".delete").on("click", function () {
+        var id = $(this).data(id);
+
+        console.log(id.id)
+
+        $.ajax("/api/tasks/" + id.id, {
+            type: "DELETE",
+            url: "/api/tasks/" + id.id
+        }).then(function () {
+            console.log(id + " has been deleted")
+            location.reload();
+        });
+    });
+
+    //handles the updating of a task
+    $(".updateValue").click(function (event) {
+        var id = $(this).data(id);
+        var selValue = $(".update_task").val();
+        console.log(id.id)
+        console.log(selValue)
+
+
+        var $updateCategory = selValue;
+        var $updateUser = $("#update-user");
+
+        var update = {
+            id: id.id,
+            category: $updateCategory
+        };
+        console.log("id updated: " + id.id);
+        console.log("category updated: " + $updateCategory);
+
+        $.ajax("/api/tasks/" + id.id, {
+            method: "PUT",
+            url: "/api/tasks/" + id.id,
+            data: update
+        }).then(function () {
+            console.log("updated");
+            window.location.reload();
+        });
+    });
+
+});
 
 
